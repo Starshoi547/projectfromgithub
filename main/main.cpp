18676cab053d66fe302d940fd274f16ab2192466
Dominik Kukhta <starshoi547@gmail.com>
2022-03-27 18:58:08 +0300

    Update main.cpp
    Yes

--- main/main.cpp
+++ main/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
+    int a, b, c, d;
 
 	cout << "Input a " << endl;
 	cin >> a;
@@ -15,15 +15,21 @@ int main()
 	cout << "Input c " << endl;
 	cin >> c;
 
+	cout << "Input d " << endl;
+	cin >> d;
 
-	if (a > b && a > c) {
+
+	if (a > b && a > c && a > d) {
 		cout << "variable 'a' is the biggest." << endl;
 	}
-	else if (b > a&& b > c) {
+	else if (b > a && b > c && b > d) {
 		cout << "variable 'b' is the biggest." << endl;
 	}
-	else{
+	else if (c > a && c > b && c > d) {
 		cout << "variable 'c' is the biggest." << endl;
+	}
+	else {
+	cout << "variable 'd' is the biggest." << endl;
 	}
 }
 
